47078b6626d0d7d67f4938c0ef91a20ef6d1e91a
Takeshi Sannomiya <takeshi.sannomiya@gmail.com>
2019-04-23 11:32:33 +0900

    added javadoc

--- src/main/java/jp/takeshi/Person.java
+++ src/main/java/jp/takeshi/Person.java
@@ -2,10 +2,11 @@ package jp.takeshi;
 
 /**
  * Class for test target
- * 
+ * @author santake
  *
  */
 public class Person {
+    
     private String id;
     private String firstName;
     private String lastName;
@@ -15,55 +16,42 @@ public class Person {
     public String getId() {
 	return id;
     }
-
     public void setId(String id) {
 	this.id = id;
     }
-
     public String getFirstName() {
 	return firstName;
     }
-
     public void setFirstName(String firstName) {
 	this.firstName = firstName;
     }
-
     public String getNickName() {
         return nickName;
     }
-
     public void setNickName(String nickName) {
         this.nickName = nickName;
     }
-
     public String getLastName() {
 	return lastName;
     }
-
     public void setLastName(String lastName) {
 	this.lastName = lastName;
     }
-
     public String getEmail() {
 	return email;
     }
-
     public void setEmail(String email) {
 	this.email = email;
     }
-    
     public void clear() {
 	this.id = "";
 	this.firstName = "";
 	this.lastName = "";
 	this.email = "";
     }
-    
-    
     public void throwingRuntimeException() {
 	throw new IllegalArgumentException("error");
     }
-    
     public void throwingException() throws Exception {
 	throw new Exception("error");
     }
--- src/main/java/jp/takeshi/Rank.java
+++ src/main/java/jp/takeshi/Rank.java
@@ -1,10 +1,21 @@
 package jp.takeshi;
 
+/**
+ * Rank information
+ * @author santake
+ *
+ */
 public enum Rank {
+    
     CAPTAIN,
+    
     CHIEF_MATE,
+    
     SECOND_MATE,
+    
     BOATSWAIN,
+    
     CHIEF_ENGINEER,
+    
     COOK
 }
